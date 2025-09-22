9ce9860bae2b6719a7cd2190e84d148320534473
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-09-23 00:07:39 +0200

    aobc-generate: generate a basic security score

--- src/cmd/aobc-generate/aobc-generate.go
+++ src/cmd/aobc-generate/aobc-generate.go
@@ -18,18 +18,15 @@ type Tuple struct {
 }
 
 var (
-	databaseFilename string = "database.yml"
-	progname         string = "aobc-generate"
+	databaseFilename     string = "database.yml"
+	dependenciesFilename string = "dependencies.md"
+	progname             string = "aobc-generate"
+	securityFilename     string = "security.md"
 )
 
 func aobcGenerate() error {
-	return aobcGenerateDependencies()
-}
-
-func aobcGenerateDependencies() error {
 	var err error
 	var root yaml.Node
-	var ofile *os.File
 
 	data, err := ioutil.ReadFile(databaseFilename)
 	if err != nil {
@@ -42,16 +39,27 @@ func aobcGenerateDependencies() error {
 		return err
 	}
 
-	if ofile, err = os.Create("dependencies.md"); err != nil {
-		return err
-	}
-	defer ofile.Close()
-
 	//validate the input
 	if len(root.Content) == 0 {
 		err = errors.New("invalid input (empty file)")
 		return err
 	}
+
+	err = aobcGenerateDependencies(dec, root)
+	if err == nil {
+		err = aobcGenerateSecurityReview(dec, root)
+	}
+	return err
+}
+
+func aobcGenerateDependencies(dec *yaml.Decoder, root yaml.Node) error {
+	var err error
+	var ofile *os.File
+
+	if ofile, err = os.Create(dependenciesFilename); err != nil {
+		return err
+	}
+	defer ofile.Close()
 
 	top := root.Content[0]
 
@@ -125,6 +133,113 @@ func aobcGenerateDependencies() error {
 													break
 												}
 												values[col.key] = "`" + entry.Content[m+1].Value + "`"
+												break
+											} else if entry.Content[m].Value == col.key {
+												//general case
+												values[col.key] = entry.Content[m+1].Value
+												break
+											}
+										}
+									}
+								}
+							}
+							for _, col := range columns {
+								fmt.Fprintf(ofile, "| %s ", textEscape(values[col.key]))
+							}
+							fmt.Fprintf(ofile, "|\n")
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func aobcGenerateSecurityReview(dec *yaml.Decoder, root yaml.Node) error {
+	var err error
+	var ofile *os.File
+
+	if ofile, err = os.Create(securityFilename); err != nil {
+		return err
+	}
+	defer ofile.Close()
+
+	top := root.Content[0]
+
+	//obtain the columns
+	var columns []Tuple
+	switch top.Kind {
+	case yaml.MappingNode:
+		for i := 0; i < len(top.Content); i += 2 {
+			if top.Content[i].Value != "SecurityColumns" {
+				continue
+			}
+			column := top.Content[i+1]
+			for _, v := range column.Content {
+				if v.Kind == yaml.MappingNode {
+					for k := 0; k < len(v.Content); k += 2 {
+						columns = append(columns, Tuple{v.Content[k].Value, v.Content[k+1].Value})
+					}
+				}
+			}
+		}
+	}
+
+	//output the table header
+	for _, title := range columns {
+		fmt.Fprintf(ofile, "| %s ", textEscape(title.value))
+	}
+	fmt.Fprintf(ofile, "|\n")
+	for _ = range columns {
+		fmt.Fprintf(ofile, "| --- ")
+	}
+	fmt.Fprintf(ofile, "|\n")
+
+	//output the entries
+	switch top.Kind {
+	case yaml.MappingNode:
+		for i := 0; i < len(top.Content); i += 2 {
+			if top.Content[i].Value != "Sections" {
+				continue
+			}
+			section := top.Content[i+1]
+			for _, v := range section.Content {
+				if v.Kind == yaml.MappingNode {
+					for k := 0; k < len(v.Content); k += 2 {
+						if v.Content[k+1].Kind == yaml.ScalarNode {
+							//new section
+							fmt.Fprintf(ofile, "| __%s__", textEscape(v.Content[k].Value))
+							for _ = range columns {
+								fmt.Fprintf(ofile, " |")
+							}
+							fmt.Fprintf(ofile, "\n")
+						} else if v.Content[k+1].Kind == yaml.SequenceNode {
+							var values map[string]string
+
+							//new entry
+							values = make(map[string]string)
+							//XXX hard-coded
+							values["component"] = v.Content[k].Value
+							for _, col := range columns {
+								for _, entry := range v.Content[k+1].Content {
+									if entry.Kind == yaml.MappingNode {
+										for m := 0; m < len(entry.Content); m += 2 {
+											//special case: security
+											if col.key == "security" && entry.Content[m].Value == col.key {
+												if entry.Content[m+1].Kind == yaml.SequenceNode {
+													var str []string
+
+													for _, w := range entry.Content[m+1].Content {
+														str = append(str, w.Value)
+													}
+													values[col.key] = strings.Join(str, ", ")
+													values["score"] = fmt.Sprintf("%d", len(str))
+													break
+												}
+												values[col.key] = entry.Content[m+1].Value
+												values["score"] = fmt.Sprintf("%d", 1)
 												break
 											} else if entry.Content[m].Value == col.key {
 												//general case
