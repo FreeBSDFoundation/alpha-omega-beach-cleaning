b2d4a46a468532203e519c7061a4cd916fd51ecd
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-05 15:43:30 +0100

    src/versions/libpcap: re-implement for reliability

--- src/versions/libpcap.c
+++ src/versions/libpcap.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
-#include <pcap.h>
+#include <string.h>
+#include <errno.h>
 
 int main(void)
 {
-#if defined(PCAP_AVAILABLE_1_10)
+	const char * progname = "libpcap";
+	const char * command = "tcpdump --version";
+	FILE * fp;
+	char buf[1024];
+	char * p;
+	char version[16];
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "libpcap version %15[^ \n]\n", version) == 1)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
 	printf("%s\n", "Sections");
 	printf("  - %s\n", "Network Libraries");
 	printf("    %s\n", "libpcap");
-	printf("    - version: 1.10.x\n");
-#endif
+	printf("    - version: %s\n", version);
 	return 0;
 }
