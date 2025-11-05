60e3610e5bbca5538b03e37e105c655ee33c659b
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-05 15:58:31 +0100

    src/versions: add openssl

---
+++ src/versions/openssl.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <openssl/crypto.h>
+
+int main(void)
+{
+	printf("%s\n", "Sections");
+	printf("  - %s\n", "Security Infrastructure");
+	printf("    %s\n", "OpenSSL");
+	printf("    - version: %u.%u.%u\n", OPENSSL_version_major(),
+			OPENSSL_version_minor(), OPENSSL_version_patch());
+	return 0;
+}
