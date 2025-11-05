279320016fb43a4a27670aa08c33ce767f0565d1
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-05 15:57:16 +0100

    src/versions: add dtc

---
+++ src/versions/dtc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void)
+{
+	const char * progname = "dtc";
+	const char * command = "dtc -v";
+	FILE * fp;
+	char buf[1024];
+	char * p;
+	char version[16];
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "Version: dtc %15[^ \n]\n", version) == 1)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
+	printf("%s\n", "Sections");
+	printf("  - %s\n", "Compilation Infrastructure");
+	printf("    %s\n", "dtc");
+	printf("    - version: %s\n", version);
+	return 0;
+}
