f5f24212a8bb909975442f26c55d574f4abc8a1e
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-05 11:59:59 +0100

    src/versions: import tentative code for libcbor

---
+++ src/versions/libcbor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "Components");
+	printf("  - %s\n", "System Libraries");
+	printf("    %s\n", "libcbor");
+	printf("    - version: %d.%d.%d\n", cbor_major_version,
+			cbor_minor_version, cbor_patch_version);
+	return 0;
+}
