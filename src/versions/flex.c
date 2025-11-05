065147fec42d152ab47bf91653a4888e36d60290
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-05 16:01:58 +0100

    src/versions: add flex

---
+++ src/versions/flex.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void)
+{
+	const char * progname = "flex";
+	const char * command = "flex --version";
+	FILE * fp;
+	char buf[1024];
+	char * p;
+	char version[16];
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "flex %15[^ \n]\n", version) == 1)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
+	printf("%s\n", "Sections");
+	printf("  - %s\n", "System Libraries");
+	printf("    %s\n", "flex");
+	printf("    - version: %s\n", version);
+	return 0;
+}
