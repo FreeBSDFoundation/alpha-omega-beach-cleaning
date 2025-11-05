50fe60fb5bd0a98f70bf9b0f71d55af869de688b
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-05 15:38:38 +0100

    src/versions: add tcpdump

---
+++ src/versions/tcpdump.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void)
+{
+	const char * progname = "tcpdump";
+	const char * command = "tcpdump --version";
+	FILE * fp;
+	char buf[1024];
+	char * p;
+	char version[16];
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "tcpdump version %15[^ \n]\n", version) == 1)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
+	printf("%s\n", "Sections");
+	printf("  - %s\n", "Network Tools");
+	printf("    %s\n", "tcpdump");
+	printf("    - version: %s\n", version);
+	return 0;
+}
