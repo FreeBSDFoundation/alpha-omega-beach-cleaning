7fdf1dd7a03b6d30ccf8196af82ebfc9cbaf4498
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-07 15:18:18 +0100

    src/versions: add zlib

---
+++ src/versions/zlib.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <zlib.h>
+
+int main(void)
+{
+	printf("%s\n", "Sections");
+	printf("  - %s\n", "System Libraries");
+	printf("    %s\n", "zlib");
+	printf("    - version: %s\n", ZLIB_VERSION);
+	return 0;
+}
