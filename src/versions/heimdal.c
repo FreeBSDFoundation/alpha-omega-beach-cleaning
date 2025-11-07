2351e3d423008c7fdc88ba6a3bee55ca99291a9d
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-07 12:19:39 +0100

    src/versions: add heimdal

---
+++ src/versions/heimdal.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void)
+{
+	const char * progname = "heimdal";
+	const char * command = "krb5-config --version";
+	FILE * fp;
+	char buf[1024];
+	char * p;
+	int release;
+	char version[16];
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "Kerberos %d release %15[^ \n]\n", &release,
+					version) == 2)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
+	printf("%s\n", "Sections");
+	printf("  - %s\n", "System Libraries");
+	printf("    %s\n", "heimdal");
+	printf("    - version: %s\n", version);
+	return 0;
+}
