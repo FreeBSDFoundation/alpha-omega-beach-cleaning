d20fc7f6173c3943bf6f706490da08a415459211
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-08 14:59:02 +0100

    src/versions: add acpi

---
+++ src/versions/acpi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void)
+{
+	const char * progname = "acpi";
+	const char * command = "iasl -? 2>&1";
+	FILE * fp;
+	char buf[1024];
+	char version[16] = "";
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "ASL+ Optimizing Compiler/Disassembler version %15[^ \n]\n", version) == 1)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
+	if(strlen(version) == 0)
+		return 3;
+	printf("%s:\n", "Sections");
+	printf("  - %s:\n", "Kernel");
+	printf("    %s:\n", "acpi(4)");
+	printf("    - version: %s\n", version);
+	return 0;
+}
