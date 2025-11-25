04ac1b2594948f270daaee356ed3d75fd20bf832
Pierre Pronchery <pierre@freebsdfoundation.org>
2025-11-25 12:51:41 +0100

    src/versions: add libexpat

---
+++ src/versions/libexpat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "common.c"
+
+int main(void)
+{
+	const char * progname = "libexpat";
+	const char * command = "/usr/bin/strings /usr/lib/libbsdxml.so";
+	FILE * fp;
+	char buf[1024];
+	char version[16] = "";
+
+	if((fp = popen(command, "r")) == NULL)
+	{
+		fprintf(stderr, "%s: %s: %s\n", progname, command,
+				strerror(errno));
+		return 2;
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(sscanf(buf, "expat_%15[^ \n]\n", version) == 1)
+		{
+			version[sizeof(version) - 1] = '\0';
+			break;
+		}
+	}
+	fclose(fp);
+	if(strlen(version) == 0)
+		return 3;
+	_report("System Libraries", progname, version);
+	return 0;
+}
