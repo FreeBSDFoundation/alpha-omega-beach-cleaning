8df30f3f047073c4a541c3825b62726d1800436f
Pierre Pronchery <pierre@freebsdfoundation.org>
2026-01-22 00:13:37 +0100

    project.conf: add a jsonld target

---
+++ tools/jsonld.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+# Copyright (c) 2025-2026 The FreeBSD Foundation
+#
+# This software was developed by Pierre Pronchery <pierre@defora.net> at Defora
+# Networks GmbH under sponsorship from the FreeBSD Foundation.
+
+
+#variables
+BOMTOOL="spdxtool"
+DEBUG="_debug"
+FIND="find"
+MKDIR="mkdir -p"
+PROGNAME="jsonld.sh"
+RM="rm -f"
+
+#functions
+#debug
+_debug() {
+	echo "$@" 1>&2
+	"$@"
+}
+
+_jsonld() {
+	target="$1"
+
+	$DEBUG $MKDIR -- "$target"				|| return 2
+	cd "$target" &&
+		$DEBUG $FIND ../pkgconfig -type f -a -name '*.pc' -print | while read filename; do
+			pkgconfig=${filename#../pkgconfig/}
+			pkgconfig=${pkgconfig%.pc}
+			spdx="$pkgconfig.jsonld"
+			PKG_CONFIG_PATH="$PWD/../pkgconfig" $DEBUG $BOMTOOL -- "$pkgconfig" > "$spdx"
+			if [ $? -ne 0 ]; then
+				$RM -- "$spdx"
+				ret=2
+			fi
+		done
+	return $ret
+}
+
+#usage
+_usage()
+{
+	echo "Usage: $PROGNAME [-c][-P prefix] target..." 1>&2
+	return 1
+}
+
+#main
+ret=0
+clean=0
+while getopts "cO:P:" name; do
+	case $name in
+		c)
+			clean=1
+			;;
+		O)
+			export "${OPTARG%%=*}"="${OPTARG#*=}"
+			;;
+		P)
+			#XXX ignore for compatibility
+			;;
+		?)
+			_usage
+			exit $?
+			;;
+	esac
+done
+shift $(($OPTIND - 1))
+if [ $# -ne 1 ]; then
+	_usage
+	exit $?
+fi
+
+#clean
+if [ $clean -ne 0 ]; then
+	exit 0
+fi
+
+#spdx
+for target in "$@"; do
+	_jsonld "$target"
+done
+exit $?
